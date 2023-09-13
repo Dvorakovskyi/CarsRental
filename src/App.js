adea26d254224004e1a092074ad58df0e7d05dab
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-09-13 17:46:18 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+const Loader = lazy(() => import("./components/Loader/Loader"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Suspense>
   );
-}
+};
 
 export default App;
